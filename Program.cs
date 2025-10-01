a6af70b73311d990cc4b2cd58afe8697da54943f
amgh <amgh@mmmi.sdu.dk>
2025-10-01 09:58:07 +0200

    Guess structure working in basic level

--- Program.cs
+++ Program.cs
@@ -4,9 +4,37 @@ string secret="hydrochlorid";
 Console.WriteLine(secret);
 
 
+// --helper 
+bool[] guesses = new bool['z'-'a']; // false -> has not been guessed
+
+// helpers
+
+int char2int (char c) {
+  return c-'a';
+}
+
+char int2char (int i) {
+  return (char)(((int)'a')+i);
+}
+
+void print_status () {
+  // guesses
+  Console.Write("Guesses: ");
+  for (int i=0 ; i<guesses.Length ; i++) {
+    if (guesses[i]) {
+      Console.Write(int2char(i));
+    }
+  }
+  Console.WriteLine("");
+}
+
+
+
+
 
 
 while (true) {
+  print_status();
   Console.WriteLine("Give us a guess: ");
   string? input = Console.ReadLine();
   if (input==null || input.Length!=1) {
@@ -15,4 +43,6 @@ while (true) {
   }
   char c = Char.ToLower(input[0]);
   Console.WriteLine(c);
+  guesses[char2int(c)]=true;
+
 }
